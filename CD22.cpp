e01c4b2608506ad9993e8e9f6bcf9e9dbfd41af4
aqizhoua <89376576+aqizhoua@users.noreply.github.com>
2023-02-22 21:10:31 +0800

    增加解法2 三个栈模拟左中右汉诺塔

--- CD22.cpp
+++ CD22.cpp
@@ -3,10 +3,13 @@
 //
 #include<iostream>
 #include <cstdio>
+#include<stack>
 using namespace std;
 
+//方法1 递归
 class HanNoTa{
-private:int cnt;
+private:
+    int cnt;
 public:
     HanNoTa(){
         cnt=0;
@@ -34,8 +37,57 @@ public:
     }
 };
 
+//方法2 3个栈
+class HanNoTa2{
+    enum state{none,l2m,m2l,m2r,r2m};
+private:
+    int cnt;
+    stack<int> LeftStack,MidStack,RightStack;
+public:
+    HanNoTa2(){
+        cnt=0;
+        LeftStack.push(13);//题目最大输入为12
+        MidStack.push(13);//题目最大输入为12
+        RightStack.push(13);//题目最大输入为12
+    }
+    ~HanNoTa2(){
+        printf("It will move %d steps.",cnt);
+    }
+    void hanNoTa(int N,string left,string mid,string right){
+        for(int i=N;i>0;--i){
+            LeftStack.push(i);
+        }
+        state record=state::none;
+        while(RightStack.size()<N+1){
+            cnt +=FTStack(record,state::l2m,state::m2l,MidStack,LeftStack,mid,left);
+            cnt +=FTStack(record,state::m2l,state::l2m,LeftStack,MidStack,left,mid);
+            cnt +=FTStack(record,state::m2r,state::r2m,RightStack,MidStack,right,mid);
+            cnt +=FTStack(record,state::r2m,state::m2r,MidStack,RightStack,mid,right);
+        }
+    }
+    int FTStack(state& s,state prev,state cur,stack<int>& FStack,stack<int>& TStack,string from,string to){
+        if(s!=prev&&FStack.top()<TStack.top()){
+            int temp = FStack.top();
+            TStack.push(temp);
+            FStack.pop();
+            printf("Move %d from %s to %s\n",temp,from.c_str(),to.c_str());
+            s=cur;
+            return 1;
+        }
+        return 0;
+    }
+
+
+
+};
+
 int main(){
-    HanNoTa h;
+//    HanNoTa h;
+//    int N;
+//    cin>>N;
+//    h.hanNoTa(N,"left","mid","right");
+
+    HanNoTa2 h;
     int N;
     cin>>N;
     h.hanNoTa(N,"left","mid","right");
